8366442a1007a46eb8f2d810060fe5ecb84d8fc9
Geno419 <ogbeidegild@gmail.com>
2023-11-02 23:56:25 +0000

    added api
    added api and chechWeather function

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WeatherApp
+weather app
+API source: https://openweathermap.org/
+            https://api.openweathermap.org/data/2.5/weather?q={city id}&appid=7af2e81ac3d7e59d7be831cd65898d46&units=metric
---
+++ main.js
@@ -0,0 +1,8 @@
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=london";
+const apiKey = "7af2e81ac3d7e59d7be831cd65898d46&";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + '&appid=${apiKey}')
+    let data = await response.json()
+    
+}
