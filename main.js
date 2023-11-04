c4a2db7267fd720943aa7b521185e25518643eab
Geno419 <ogbeidegild@gmail.com>
2023-11-04 00:20:26 +0000

    major improvements
    fixed search function, added notes to html and css including fixes and updates

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
-const apiKey = "7af2e81ac3d7e59d7be831cd65898d46&";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey = "7af2e81ac3d7e59d7be831cd65898d46";
 
-//const locationSeacrh = document.querySelector('.search input');
-//const locationSeacrhBTN = document.querySelector('.search button');
+const locationSearch = document.querySelector(".searchBarContainer input");
+const searchBTN = document.querySelector(".searchBarContainer button");
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + 'london' + '&appid=' + apiKey);
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + '&appid=' + apiKey);
     let data = await response.json();
     
     console.log(data);
 
     document.querySelector(".location").innerHTML= data.name;
-    document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + '°c';
-    document.querySelector(".humidity").innerHTML= data.main.humidity + '%';
-    document.querySelector(".wind").innerHTML= data.wind.speed + ' km/h';
-
+    document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + '&#8451';
+    document.querySelector(".maxTemp").innerHTML= data.wind.speed + '&#37';
+    document.querySelector(".minTemp").innerHTML= 'min: ' + data.main.temp_min + '&#8451 ';
+    document.querySelector(".maxTemp").innerHTML= 'max: ' + data.main.temp_max + '&#8451';
+    document.querySelector(".humidity").innerHTML= data.main.humidity + '&#37';
+    document.querySelector(".weatherDesc").innerHTML= data.weather[0].description;
+    
 }
-locationSeacrhBTN.addEventListener('click', ()=>{
-    checkWeather(searchBox.value);
+searchBTN.addEventListener("click", ()=>{
+    checkWeather(locationSearch.value);
 })
+
 checkWeather();
