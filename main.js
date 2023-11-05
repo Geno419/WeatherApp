edff87ad37841fbd8f887e909e9a44c8a07ceb46
Geno419 <ogbeidegild@gmail.com>
2023-11-05 21:36:58 +0000

    Revert "Merge branch 'main' of https://github.com/Geno419/WeatherApp"
    This reverts commit 4ce3ea1bbf69a1a7aca62823dfa46dccc4bb19f3, reversing
    changes made to 8150de2069d387c6fcdc469d0d78acf517e39ceb.

--- main.js
+++ main.js
@@ -1,44 +1,22 @@
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const apiKey = "7af2e81ac3d7e59d7be831cd65898d46";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const apiKey = "7af2e81ac3d7e59d7be831cd65898d46&";
 
-const locationSearch = document.querySelector(".searchBarContainer input");
-const searchBtn = document.querySelector(".searchBarContainer button");
+//const locationSeacrh = document.querySelector('.search input');
+//const locationSeacrhBTN = document.querySelector('.search button');
 
-async function checkWeather(city){
-    const response = await fetch(apiUrl + city + '&appid=' + apiKey);
+async function checkWeather(){
+    const response = await fetch(apiUrl + 'london' + '&appid=' + apiKey);
     let data = await response.json();
     
     console.log(data);
 
     document.querySelector(".location").innerHTML= data.name;
-    document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + '&#8451';
-    document.querySelector(".maxTemp").innerHTML= data.wind.speed + '&#37';
-    document.querySelector(".minTemp").innerHTML= 'min: ' + data.main.temp_min + '&#8451 ';
-    document.querySelector(".maxTemp").innerHTML= 'max: ' + data.main.temp_max + '&#8451';
-    document.querySelector(".humidity").innerHTML= data.main.humidity + '&#37';
-    document.querySelector(".weatherDesc").innerHTML= data.weather[0].description;
-    document.querySelector('.date').innerHTML= new Date(new Date().getTime() + data.timezone * 1000).toLocaleDateString('en-GB', { 
- 
-      //weekday: 'long',
-      //year: 'numeric', 
-      month: 'long', 
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      //second: 'numeric',
-      hour12: true,});
-    
+    document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + '°c';
+    document.querySelector(".humidity").innerHTML= data.main.humidity + '%';
+    document.querySelector(".wind").innerHTML= data.wind.speed + ' km/h';
+
 }
-
-locationSearch.addEventListener("keydown", (event) =>{
-    if (event.key === "Enter") {
-      event.preventDefault(); 
-      checkWeather(locationSearch.value);
-    }
-  });
-
-searchBtn.addEventListener("click", ()=>{
-    checkWeather(locationSearch.value);
+locationSeacrhBTN.addEventListener('click', ()=>{
+    checkWeather(searchBox.value);
 })
-
-checkWeather("London");
+checkWeather();
