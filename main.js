50611c7b69cc51cd3c6b520bfb3885ad38ce3a63
Geno419 <ogbeidegild@gmail.com>
2023-11-05 21:44:49 +0000

    updated

--- main.js
+++ main.js
@@ -11,12 +11,33 @@ async function checkWeather(){
     console.log(data);
 
     document.querySelector(".location").innerHTML= data.name;
+<<<<<<< HEAD
     document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + '°c';
     document.querySelector(".humidity").innerHTML= data.main.humidity + '%';
     document.querySelector(".wind").innerHTML= data.wind.speed + ' km/h';
 
+=======
+    document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + '&#8451';
+    document.querySelector(".maxTemp").innerHTML= data.wind.speed + '&#37';
+    document.querySelector(".minTemp").innerHTML= 'min: ' + data.main.temp_min + '&#8451 ';
+    document.querySelector(".maxTemp").innerHTML= 'max: ' + data.main.temp_max + '&#8451';
+    document.querySelector(".humidity").innerHTML= data.main.humidity + '&#37';
+    document.querySelector(".weatherDesc").innerHTML= data.weather[0].description;
+
+    
+>>>>>>> parent of 4e3f8cc (major improvements with the time)
 }
 locationSeacrhBTN.addEventListener('click', ()=>{
     checkWeather(searchBox.value);
 })
+<<<<<<< HEAD
 checkWeather();
+=======
+
+<<<<<<< HEAD
+checkWeather();
+
+=======
+checkWeather();
+>>>>>>> parent of 4eeb28c (all set)
+>>>>>>> parent of 4e3f8cc (major improvements with the time)
