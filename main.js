38443679fe2748a0b9997f532eeaff57301cd4a8
Geno419 <ogbeidegild@gmail.com>
2023-11-06 19:46:54 +0000

    Completed all of the Js
    Completed all of the Js and added small to the css and html

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const apiKey = "7af2e81ac3d7e59d7be831cd65898d46";
 
-const locationSearch = document.querySelector(".searchBarContainer input");
-const searchBtn = document.querySelector(".searchBarContainer button");
+const locationSearch = document.querySelector(".searchBar input");
+const searchBtn = document.querySelector(".searchBar button");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + '&appid=' + apiKey);
     let data = await response.json();
-    
-    console.log(data);
 
-   // document.querySelector(".location").innerHTML= data.name;//
+    console.log(data);
+    
     document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + '&#8451';
     document.querySelector(".maxTemp").innerHTML= data.wind.speed + '&#37';
     document.querySelector(".minTemp").innerHTML= 'min: ' + data.main.temp_min + '&#8451 ';
     document.querySelector(".maxTemp").innerHTML= 'max: ' + data.main.temp_max + '&#8451';
     document.querySelector(".humidity").innerHTML= data.main.humidity + '&#37';
     document.querySelector(".weatherDesc").innerHTML= data.weather[0].description;
+
+    let weatherMain = data.weather[0].main.toLowerCase();
+    document.querySelector(".weatherIcon").src = 'images/' + weatherMain + '.png'
+    
     document.querySelector('.date').innerHTML= new Date(new Date().getTime() + data.timezone * 1000).toLocaleDateString('en-GB', { 
- 
       //weekday: 'long',
       //year: 'numeric', 
       month: 'long', 
@@ -26,8 +28,9 @@ async function checkWeather(city){
       hour: 'numeric',
       minute: 'numeric',
       //second: 'numeric',
-      hour12: true,});
-    
+      hour12: true,
+    });
+
 }
 
 locationSearch.addEventListener("keydown", (event) =>{
@@ -41,4 +44,4 @@ searchBtn.addEventListener("click", ()=>{
     checkWeather(locationSearch.value);
 })
 
-checkWeather("London");
+checkWeather('');
